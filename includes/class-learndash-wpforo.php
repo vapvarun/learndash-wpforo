65576f4d0105717f306c45b89626fd8d72b9b398
Wbcom Dev <pallavi@wbcomdesigns.com>
2023-07-04 11:03:44 +0530

    (#44)Courses inside the board forum

--- includes/class-learndash-wpforo.php
+++ includes/class-learndash-wpforo.php
@@ -174,6 +174,8 @@ class Learndash_Wpforo {
 		// $this->loader->add_action( 'forums_page_wpforo-forums', $plugin_admin,'ldwpforo_display_course_selector', 20 );
 		$this->loader->add_action( 'wpforo_clean_cache_start', $plugin_admin, 'ldwpforo_add_edit_ldwpforo_settings', 20 );
 		 $this->loader->add_action( 'admin_init', $plugin_admin, 'wbcom_hide_all_admin_notices_from_setting_page' );
+		 $this->loader->add_action( 'admin_footer', $plugin_admin, 'wpforo_course_integrate_in_board' );
+		 $this->loader->add_action( 'wpforo_clean_cache_start', $plugin_admin, 'wpforo_board_forum_course_setting_save', 20 );
 	}
 
 	/**
